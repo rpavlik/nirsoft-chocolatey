078315959537536186d124396d1c8a8d434442c6
Ryan Pavlik <ryan@sensics.com>
2015-04-21 12:15:59 -0500

    Add shellexview

---
+++ shellexview/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'ShellExView'
+
+$checksumType = 'sha1'
+
+$url = 'http://www.nirsoft.net/utils/shexview.zip'
+$checksum = '41DE6F2448FDAACA987CDC461EDCBAC4102CAE78'
+
+$url64 = 'http://www.nirsoft.net/utils/shexview-x64.zip'
+$checksum64 = 'EDE598083ECE04C9C1A72F7C465A5243A785CA7F'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# Install the zip file - checksums are required since the download doesn't have a version in its name.
+Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir"  -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType
+
+# Create the dummy file to indicate a GUI shim needed
+New-Item -ItemType file $(Join-Path $toolsDir 'shexview.exe.gui')
