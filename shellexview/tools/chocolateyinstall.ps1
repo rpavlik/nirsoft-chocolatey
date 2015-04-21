bfe57b455efb0b1f2e43464f16fb71282ed8887a
Ryan Pavlik <ryan@sensics.com>
2015-04-21 13:11:50 -0500

    Re-arrange shellexview script to match the shellmenuview script

--- shellexview/tools/chocolateyinstall.ps1
+++ shellexview/tools/chocolateyinstall.ps1
@@ -1,19 +1,18 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$packageName = 'ShellExView'
+$shortName = 'shexview' # used to create the download URL and the exe shim dummy file.
+$checksum = '41DE6F2448FDAACA987CDC461EDCBAC4102CAE78' # 32-bit download's sha1
+$checksum64 = 'EDE598083ECE04C9C1A72F7C465A5243A785CA7F' # 64-bit download's sha1
 
-$packageName = 'ShellExView'
+# Things following this line should be relatively universal for NirSoft tools.
+$url = "http://www.nirsoft.net/utils/$shortName.zip"
+$url64 = "http://www.nirsoft.net/utils/$shortName-x64.zip"
 
+$ErrorActionPreference = 'Stop';
 $checksumType = 'sha1'
-
-$url = 'http://www.nirsoft.net/utils/shexview.zip'
-$checksum = '41DE6F2448FDAACA987CDC461EDCBAC4102CAE78'
-
-$url64 = 'http://www.nirsoft.net/utils/shexview-x64.zip'
-$checksum64 = 'EDE598083ECE04C9C1A72F7C465A5243A785CA7F'
-
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # Install the zip file - checksums are required since the download doesn't have a version in its name.
 Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir" "$url64" -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType
 
 # Create the dummy file to indicate a GUI shim needed
-New-Item -ItemType file $(Join-Path "$toolsDir" 'shexview.exe.gui')
+New-Item -ItemType file $(Join-Path "$toolsDir" "$shortName.exe.gui")
