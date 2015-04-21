58cf63e3349659f5619cab9861a6b723c657c6ee
Ryan Pavlik <ryan@sensics.com>
2015-04-21 13:49:07 -0500

    Update shellmenuview with the same changes

---
+++ shellmenuview/tools/config.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = 'ShellMenuView'
+$shortName = 'shmnview' # used to create the download URL and the exe shim dummy file.
+$checksum = '94533F2BCB1757D864BA544DAC9A472DB8FC09AD' # 32-bit download's sha1
+$checksum64 = 'F190ADA098D8BCE3697D0FEEC8DB8061CE69F8F2' # 64-bit download's sha1
